1ba7b47e4f929d18e01efbc5e421423f78f645e1
david.liu <liuchong1228@yahoo.cn>
2014-02-25 16:51:40 +0800

    first commit

---
+++ cjzyb_ios/Tools/Const.h
@@ -0,0 +1,16 @@
+//
+//  Const.h
+//  cjzyb_ios
+//
+//  Created by david on 14-2-25.
+//  Copyright (c) 2014年 david. All rights reserved.
+//
+
+#ifndef cjzyb_ios_Const_h
+#define cjzyb_ios_Const_h
+
+
+
+#endif
+
+#import "Reachability.h"
