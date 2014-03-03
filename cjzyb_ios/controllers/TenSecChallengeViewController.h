056f0de9e25a8b55dd85206d11a87ca0901c72cc
lhl0033 <lhl0033@gmail.com>
2014-03-03 10:45:34 +0800

    十全:添加约束之前

---
+++ cjzyb_ios/controllers/TenSecChallengeViewController.h
@@ -0,0 +1,13 @@
+//
+//  TenSecChallengeViewController.h
+//  cjzyb_ios
+//
+//  Created by apple on 14-3-3.
+//  Copyright (c) 2014年 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TenSecChallengeViewController : UIViewController
+
+@end
